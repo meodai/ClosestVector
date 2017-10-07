fcc866d5157f632499a8381a455dc2721d2b518d
David Aerne <meodai@gmail.com>
2017-10-07 22:47:25 +0200

    adds more tests

--- test.js
+++ test.js
@@ -37,4 +37,46 @@ describe('Closest', () => {
     const a = new Array(100).fill('1');
     expect(Closest.getDimensions(a)).to.be.equal(a.length);
   });
+
+  it('Closest.oneDimensionalDiff to diff simple numbers correctly', () => {
+    expect(Closest.oneDimensionalDiff(0,10)).to.be.equal(10);
+    expect(Closest.oneDimensionalDiff(-10,10)).to.be.equal(20);
+    expect(Closest.oneDimensionalDiff(-20,-1)).to.be.equal(19);
+  });
+
+  it('Closest.oneDimensionalDiff to diff simple numbers correctly', () => {
+    expect(Closest.oneDimensionalDiff(0,10)).to.be.equal(10);
+    expect(Closest.oneDimensionalDiff(-10,10)).to.be.equal(20);
+    expect(Closest.oneDimensionalDiff(-20,-1)).to.be.equal(19);
+    expect(Closest.oneDimensionalDiff(0,0)).to.be.equal(0);
+    expect(Closest.oneDimensionalDiff(0.1,0.000001)).to.be.equal(0.099999);
+  });
+
+  it('Closest.nDimensionalDiff to return correct difference between two Vector2', () => {
+    expect(Closest.nDimensionalDiff([10,10],[10,10])).to.be.equal(0);
+    expect(Closest.nDimensionalDiff([10,10],[100,100])).to.be.equal(
+      Math.sqrt(
+        (10 - 100) * (10 - 100) +
+        (10 - 100) * (10 - 100)
+      )
+    );
+
+    expect(Closest.nDimensionalDiff([-1,1],[100,100])).to.be.equal(
+      Math.sqrt(
+        (-1 - 100) * (-1 - 100) +
+        (1 - 100) * (1 - 100)
+      )
+    );
+  });
+
+  it('Closest.nDimensionalDiff to return correct difference between two Vector3', () => {
+    expect(Closest.nDimensionalDiff([255,255,255],[255,255,255])).to.be.equal(0);
+    expect(Closest.nDimensionalDiff([255,255,255],[0,0,0])).to.be.equal(
+      Math.sqrt(
+        (255 - 0) * (255 - 0) +
+        (255 - 0) * (255 - 0) +
+        (255 - 0) * (255 - 0)
+      )
+    );
+  });
 })
